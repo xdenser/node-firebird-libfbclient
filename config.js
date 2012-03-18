89d490687092b5a678f344776e05f94e70d0535e
xdenser <xdenser@gmail.com>
2012-03-19 00:34:34 +0200

    remove config.js from root

--- config.js
+++
@@ -1,8 +0,0 @@
-exports.cfg = {
-  // Database connection settings
-    db: "localhost:test4.fdb",
-      user: "sysdba",
-        password: "masterkey",
-          role: ""
-          };
-          
