1761a1a5276ab6d8a881dd8e7e5e276808e9ea3b
Denys Khanzhiyev <xdenser@gmail.com>
2014-10-10 23:46:13 -0700

    fb-bindings and result

--- src/fb-bindings-fbresult.h
+++ src/fb-bindings-fbresult.h
@@ -33,7 +33,7 @@ public:
  static bool prepare_sqlda(XSQLDA *sqlda);
  static void clean_sqlda(XSQLDA *sqlda);
  static bool clone_sqlda(XSQLDA *src_sqlda,XSQLDA **dest_sqlda);
- static Handle<Value> set_params(XSQLDA *sqlda, const Arguments& args);
+ static Handle<Value> set_params(XSQLDA *sqlda, _NAN_METHOD_ARGS_TYPE args);
  Local<Object> getCurrentRow(bool asObject);
    
 protected:  
