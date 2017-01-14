a3eefe5ec4603e002d2f570b9132048b9ef0febe
Denys Khanzhyiev <xdenser@gmail.com>
2017-01-14 19:30:19 +0100

    correct testdb creation sql

--- tests/createTestDB.sql
+++ tests/createTestDB.sql
@@ -1,5 +1,2 @@
-create database 'test.fdb'
-USER 'SYSDBA' PASSWORD 'masterkey'
-page_size 8192
-DEFAULT CHARACTER SET UTF8;
+create database 'test.fdb' USER 'SYSDBA' PASSWORD 'masterkey' page_size 8192 DEFAULT CHARACTER SET UTF8;
 SET SQL DIALECT 3;
