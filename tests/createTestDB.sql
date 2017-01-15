a5066c8a642a61cbf76ccfcfd0c6ae543e98bab4
Denys Khanzhiyev <xdenser@gmail.com>
2017-01-15 16:51:21 +0100

    Update createTestDB.sql

--- tests/createTestDB.sql
+++ tests/createTestDB.sql
@@ -1,2 +1,2 @@
-create database 'test.fdb' USER 'SYSDBA' PASSWORD 'masterkey' page_size 8192 DEFAULT CHARACTER SET UTF8;
 SET SQL DIALECT 3;
+create database 'TEST.FDB' USER 'SYSDBA' PASSWORD 'masterkey' page_size 8192 DEFAULT CHARACTER SET UTF8;
