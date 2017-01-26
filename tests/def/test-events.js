16e86303cc71aa567f385bb1f386f5526ab276d5
Jacob Alberty <jacob.alberty@gmail.com>
2017-01-26 01:49:37 -0600

    New more complex AddAndDeleteMultiComplex
    Could probably use random shuffling as well but for now this triggers the same crash.

--- tests/def/test-events.js
+++ tests/def/test-events.js
@@ -270,15 +270,17 @@ exports.AddAndDeleteMultiComplex = function(test){
     called = true;
   });
   var eN = 'eventName';
-  conn.addFBevent(eN + 1);
-  conn.addFBevent(eN + 2);
-  conn.addFBevent(eN + 3);
-  conn.addFBevent(eN + 10);
-  conn.deleteFBevent(eN + 3);
-  conn.deleteFBevent(eN + 2);
-  conn.deleteFBevent(eN + 10);
-  conn.deleteFBevent(eN + 1);
-  GenEvent(eN + 1);
+  conn.addFBevent(eN);
+  var evs = [...Array(20).keys()];
+  for (i in evs) {
+    conn.addFBevent(eN+i);
+  }
+  for (i in evs) {
+    conn.deleteFBevent(eN+i);
+  }
+
+  conn.deleteFBevent(eN);
+  GenEvent(eN);
 
   setTimeout(function(){
        test.ok(!called,"Event not called");
